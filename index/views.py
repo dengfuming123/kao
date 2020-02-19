8d3d85def250fc989bfa6f48bc574d94e2de79be
dengfuming123 <dfm1998@qq.com>
2020-02-19 22:52:30 +0800

    翻页

--- index/views.py
+++ index/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, request
 from .models import Post, Commit
+from   django.db.models    import   Q
+from django.db.models import Count
 from index import  models
 from datetime import datetime
 # Create your views here.
 def index(request):
-  # 获取当前请求的用户名
-  username = request.user.username
-  posts = Post.objects.all()
+  # # 获取当前请求的用户名
+  # username = request.user.username
+  # posts = Post.objects.all()
+  # post_count = Post.objects.aggregate(Count('id'))
+  # list_num = []
   # now = datetime.now
-  user_id = request.user.id
-  if username == 'qq792074582':
-      superuser = 1
-  else:
-      superuser = None
+  # user_id = request.user.id
+  # if username == 'qq792074582':
+  #     superuser = 1
+  # else:
+  #     superuser = None
   return render(request, 'index.html', locals())
-
+def pagechange(request, n):
+    username = request.user.username
+    max = int(n) #转化为 int类型
+    posts = Post.objects.filter(Q(id__lte=max*10) & Q(id__gte=(max-1)*10+1))
+    post_count = Post.objects.aggregate(Count('id'))
+    numberpage = 1+post_count['id__count']//10
+    countpage = numberpage+1
+    list_num = []
+    now = datetime.now
+    user_id = request.user.id
+    if username == 'qq792074582':
+        superuser = 1
+    else:
+        superuser = None
+    for i in range(1, countpage):
+        list_num.append(i)
+    return render(request, 'page.html', locals())
 def showpost(request,id):
     # try:
     post = Post.objects.get(id=id)
